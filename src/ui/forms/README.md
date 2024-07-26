5b9f2d943e73a3f44972c3ecc60ad44be0fe8512
Joao Coelho <joaonc@hotmail.com>
2024-07-26 18:22:50 -0500

    ui

--- src/ui/forms/README.md
+++ src/ui/forms/README.md
@@ -1,5 +1,26 @@
 # `src/ui/forms` folder
-This folder contains auto-generated files from the `.ui` files under `assets/ui`.
+The files in this folder are created with PySide tools and should not be edited manually or with
+linters.
 
-These files are created with `pyside6-uic`, usually via the command `inv ui.py` and should not be
-edited either manually or with linters.
+## `ui_*.py` files
+Generated from the `.ui` files under `/assets/ui`.
+
+Uses the tool `pyside6-uic` and generated with the command
+```
+inv ui.py -f <filename>
+
+# Ex
+inv ui.py -f show_dialog
+```
+
+`inv --help ui.py` for more details.
+
+## `resources_rc.py` file
+Resources added in the UI builder.
+
+Generated from the `.qrc` files under `/assets`.
+
+Uses the tool `pyside6-rcc` and generated with the command
+```
+inv ui.rc
+```
--- src/ui/forms/ui_show_dialog.py
+++ src/ui/forms/ui_show_dialog.py
@@ -15,17 +15,37 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QPushButton,
+    QSizePolicy, QWidget)
 from . import resources_rc
 
 class Ui_ShowDialog(object):
     def setupUi(self, ShowDialog):
         if not ShowDialog.objectName():
             ShowDialog.setObjectName(u"ShowDialog")
-        ShowDialog.resize(400, 300)
+        ShowDialog.resize(677, 584)
+        font = QFont()
+        font.setPointSize(50)
+        ShowDialog.setFont(font)
         icon = QIcon()
         icon.addFile(u":/images/window_icon.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         ShowDialog.setWindowIcon(icon)
+        self.title_label = QLabel(ShowDialog)
+        self.title_label.setObjectName(u"title_label")
+        self.title_label.setGeometry(QRect(120, 50, 431, 121))
+        self.title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.details_label = QLabel(ShowDialog)
+        self.details_label.setObjectName(u"details_label")
+        self.details_label.setGeometry(QRect(300, 260, 261, 101))
+        font1 = QFont()
+        font1.setPointSize(20)
+        self.details_label.setFont(font1)
+        self.pass_button = QPushButton(ShowDialog)
+        self.pass_button.setObjectName(u"pass_button")
+        self.pass_button.setGeometry(QRect(330, 430, 331, 121))
+        self.fail_button = QPushButton(ShowDialog)
+        self.fail_button.setObjectName(u"fail_button")
+        self.fail_button.setGeometry(QRect(30, 430, 271, 121))
 
         self.retranslateUi(ShowDialog)
 
@@ -34,5 +54,9 @@ class Ui_ShowDialog(object):
 
     def retranslateUi(self, ShowDialog):
         ShowDialog.setWindowTitle(QCoreApplication.translate("ShowDialog", u"Show Dialog", None))
+        self.title_label.setText(QCoreApplication.translate("ShowDialog", u"Title", None))
+        self.details_label.setText(QCoreApplication.translate("ShowDialog", u"Details", None))
+        self.pass_button.setText(QCoreApplication.translate("ShowDialog", u"Pass", None))
+        self.fail_button.setText(QCoreApplication.translate("ShowDialog", u"Fail", None))
     # retranslateUi
 
