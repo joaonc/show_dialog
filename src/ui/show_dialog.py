58eab51b297368bc3693854c1648559271bc7066
Joao Coelho <joaonc@users.noreply.github.com>
2024-07-27 09:57:04 -0700

    Pass/Fail UI (#3)

--- src/ui/show_dialog.py
+++ src/ui/show_dialog.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QApplication
 
 from src.ui.forms.ui_show_dialog import Ui_ShowDialog
 
@@ -7,3 +7,18 @@ class ShowDialog(QMainWindow, Ui_ShowDialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # UI adjustments
+        self.pass_button.setIconSize(self.pass_button.size())
+        self.fail_button.setIconSize(self.fail_button.size())
+
+        # UI bindings
+        self.pass_button.clicked.connect(self.pass_clicked)
+        self.fail_button.clicked.connect(self.fail_clicked)
+
+    def pass_clicked(self):
+        self.close()
+
+    # noinspection PyMethodMayBeStatic
+    def fail_clicked(self):
+        QApplication.exit(1)
