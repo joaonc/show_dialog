f9a20cfb0e0d060465353e03395f07524e9c15b3
Joao Coelho <joaonc@hotmail.com>
2024-07-29 07:31:43 -0500

    ui updates

--- src/ui/show_dialog.py
+++ src/ui/show_dialog.py
@@ -1,19 +1,17 @@
 from PySide6.QtGui import QKeySequence, QShortcut
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QDialog
 
 from src.inputs import Inputs
 from src.ui.forms.ui_show_dialog import Ui_ShowDialog
 
 
-class ShowDialog(QMainWindow, Ui_ShowDialog):
+class ShowDialog(QDialog, Ui_ShowDialog):
     def __init__(self, inputs: Inputs):
         super().__init__()
         self.setupUi(self)
         self.inputs = inputs
 
         # UI adjustments
-        self.pass_button.setIconSize(self.pass_button.size())
-        self.fail_button.setIconSize(self.fail_button.size())
         self.title_label.setText(self.inputs.title)
         self.description_label.setText(self.inputs.description)
         if self.inputs.dialog_title:
@@ -28,6 +26,10 @@ class ShowDialog(QMainWindow, Ui_ShowDialog):
         self.fail_button.clicked.connect(self.fail_clicked)
         self.exit_shortcut = QShortcut(QKeySequence('Ctrl+Q'), self)
         self.exit_shortcut.activated.connect(self.fail_clicked)
+
+    def resizeEvent(self, event):
+        self.pass_button.setIconSize(self.pass_button.size())
+        self.fail_button.setIconSize(self.fail_button.size())
 
     # noinspection PyMethodMayBeStatic
     def pass_clicked(self):
