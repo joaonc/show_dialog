c3b6122f411e7b503949af95c80fe2f829b02485
Joao Coelho <joaonc@hotmail.com>
2024-07-29 21:45:46 -0500

    tests

--- src/inputs.py
+++ src/inputs.py
@@ -1,5 +1,6 @@
 import json
 from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 from typing import Any, Type, TypeVar
 
@@ -9,31 +10,60 @@ from mashumaro.mixins.json import DataClassJSONMixin
 T = TypeVar('T', bound='JSONFileMixin')
 
 
+class DataFileType(Enum):
+    AUTO = 'AUTO'
+    """Use file type based on file extension."""
+    JSON = 'JSON'
+    YAML = 'YAML'
+
+    @classmethod
+    def from_file(cls, file: str | Path) -> 'DataFileType':
+        file_path = Path(file)
+        if file_path.suffix.lower() == '.json':
+            file_type = DataFileType.JSON
+        elif file_path.suffix.lower() in ['.yaml', '.yml']:
+            file_type = DataFileType.YAML
+        else:
+            raise ValueError(f'Unsupported file type: {file}')
+
+        return file_type
+
+
 class JSONFileMixin(DataClassJSONMixin):
-    def to_json_file(self, file: str | Path, **from_dict_kwargs: Any):
-        file_path = Path(file)
-        data = self.to_dict(**from_dict_kwargs)
-        with open(file_path, 'w') as f:
-            json.dump(data, f, indent=2)
 
-    def to_yaml_file(self, file: str | Path, **from_dict_kwargs: Any):
+    def to_file(
+        self,
+        file: str | Path,
+        file_type: DataFileType = DataFileType.AUTO,
+        **from_dict_kwargs: Any,
+    ):
         file_path = Path(file)
+        if file_type is DataFileType.AUTO:
+            file_type = DataFileType.from_file(file_path)
         data = self.to_dict(**from_dict_kwargs)
+
         with open(file_path, 'w') as f:
-            yaml.safe_dump(data, f, default_flow_style=False, indent=2)
+            if file_type is DataFileType.JSON:
+                json.dump(data, f, indent=2)
+            else:
+                yaml.safe_dump(data, f, default_flow_style=False, indent=2)
 
     @classmethod
-    def from_json_file(cls: Type[T], file: str | Path, **from_dict_kwargs: Any) -> T:
+    def from_file(
+        cls: Type[T],
+        file: str | Path,
+        file_type: DataFileType = DataFileType.AUTO,
+        **from_dict_kwargs: Any,
+    ) -> T:
         file_path = Path(file)
-        with open(file_path) as f:
-            data = json.load(f)
-        return cls.from_dict(data, **from_dict_kwargs)
+        if file_type is DataFileType.AUTO:
+            file_type = DataFileType.from_file(file_path)
 
-    @classmethod
-    def from_yaml_file(cls: Type[T], file: str | Path, **from_dict_kwargs: Any) -> T:
-        file_path = Path(file)
         with open(file_path) as f:
-            data = yaml.safe_load(f)
+            if file_type is DataFileType.JSON:
+                data = json.load(f)
+            else:
+                data = yaml.safe_load(f)
         return cls.from_dict(data, **from_dict_kwargs)
 
 
--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -3,7 +3,7 @@ import json
 import pytest
 import yaml
 
-from src.inputs import Inputs
+from src.inputs import DataFileType, Inputs
 
 
 @pytest.fixture
@@ -11,46 +11,79 @@ def inputs_instance():
     return Inputs(title='Foo', description='Bar')
 
 
-def test_instantiation():
-    inputs = Inputs(title='Foo', description='Bar')
-
-    assert inputs.title == 'Foo'
-    assert inputs.description == 'Bar'
-
-
-def test_to_json_file(tmp_path, inputs_instance):
-    file = tmp_path / 'test.json'
-    inputs_instance.to_json_file(file)
-
-    with open(file) as f:
-        data = json.load(f)
-
-    inputs_instance_2 = Inputs.from_dict(data)
-    assert inputs_instance == inputs_instance_2
+class TestDataFileType:
+    @pytest.mark.parametrize(
+        'file, expected',
+        [
+            ('foo.json', DataFileType.JSON),
+            ('foo.JSON', DataFileType.JSON),
+            ('foo.yaml', DataFileType.YAML),
+            ('foo.yMl', DataFileType.YAML),
+        ],
+    )
+    def test_from_file(self, file, expected):
+        assert DataFileType.from_file(file) is expected
 
 
-def test_to_yaml_file(tmp_path, inputs_instance):
-    file = tmp_path / 'test.yaml'
-    inputs_instance.to_yaml_file(file)
+class TestInputs:
+    def test_instantiation(self):
+        inputs = Inputs(dialog_title='Baz', title='Foo', description='Bar')
 
-    with open(file) as f:
-        data = yaml.safe_load(f)
+        assert inputs.dialog_title == 'Baz'
+        assert inputs.title == 'Foo'
+        assert inputs.description == 'Bar'
 
-    inputs_instance_2 = Inputs.from_dict(data)
-    assert inputs_instance == inputs_instance_2
+    def test_instantiation_defaults(self):
+        inputs = Inputs()
 
+        assert inputs.dialog_title == ''
+        assert inputs.title == ''
+        assert inputs.description == ''
 
-def test_from_json_file(tmp_path, inputs_instance):
-    file = tmp_path / 'test.json'
-    inputs_instance.to_json_file(file)
+    @pytest.mark.parametrize(
+        'file_name, file_type',
+        [
+            ('test.json', DataFileType.JSON),
+            ('test.yaml', DataFileType.YAML),
+            ('test.json', DataFileType.AUTO),
+            ('test.yaml', DataFileType.AUTO),
+            ('test.yml', DataFileType.AUTO),
+        ],
+    )
+    def test_to_file(self, tmp_path, inputs_instance, file_name, file_type):
+        file = tmp_path / file_name
+        inputs_instance.to_file(file)
 
-    inputs_instance_2 = Inputs.from_json_file(file)
-    assert inputs_instance == inputs_instance_2
+        with open(file) as f:
+            open_func = (
+                json.load if DataFileType.from_file(file) is DataFileType.JSON else yaml.safe_load
+            )
+            data = open_func(f)
 
+        inputs_instance_2 = Inputs.from_dict(data)
+        assert inputs_instance == inputs_instance_2
 
-def test_from_yaml_file(tmp_path, inputs_instance):
-    file = tmp_path / 'test.yaml'
-    inputs_instance.to_yaml_file(file)
+    def test_to_file_invalid(self, tmp_path, inputs_instance):
+        file = tmp_path / 'foo.bar'
+        with pytest.raises(ValueError):
+            inputs_instance.to_file(file)
 
-    inputs_instance_2 = Inputs.from_yaml_file(file)
-    assert inputs_instance == inputs_instance_2
+    @pytest.mark.parametrize(
+        'file_name',
+        [
+            'test.json',
+            'test.yaml',
+            'test.yml',
+        ],
+    )
+    def test_from_file(self, tmp_path, inputs_instance, file_name):
+        file = tmp_path / file_name
+        inputs_instance.to_file(file)
+
+        inputs_instance_2 = Inputs.from_file(file)
+        assert inputs_instance == inputs_instance_2
+
+    def test_from_file_invalid(self, tmp_path, inputs_instance):
+        file = tmp_path / 'foo.bar'
+        with pytest.raises(ValueError):
+            Inputs.from_file(file)
