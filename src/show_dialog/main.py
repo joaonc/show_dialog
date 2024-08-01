ce7c8c1b37359d8d024dbdff6f23eaab3fd58c98
Joao Coelho <joaonc@users.noreply.github.com>
2024-08-01 15:37:48 -0700

    Remove update functionality (#15)

--- src/show_dialog/main.py
+++ src/show_dialog/main.py
@@ -3,11 +3,10 @@ import os
 import pprint
 import sys
 import types
-from pathlib import Path
 
 from PySide6.QtWidgets import QApplication
 
-from src.show_dialog import config, update
+from src.show_dialog import config
 from src.show_dialog.inputs import Inputs
 from src.show_dialog.ui.show_dialog import ShowDialog
 
@@ -17,51 +16,20 @@ INPUTS = os.environ.get(
 
 
 def main(inputs: Inputs, stylesheet: str | None):
-    # Launch UI
-    if not config.check_update_only:
-        app = QApplication()
-        window = ShowDialog(app, inputs, stylesheet)
-        window.show()
-        app_response = app.exec()
-        if app_response != 0:
-            logging.error(f'An error occurred. Exiting with status {app_response}.')
-            sys.exit(app_response)
-
-    # Update
-    if config.check_update or config.check_update_only:
-        try:
-            need_update, version_update = update.check_update(config.update_manifest)
-            if version_update is None:
-                logging.warning('Manifest file to update not specified.')
-        except FileNotFoundError:
-            logging.warning(
-                f'Manifest for file to update not found: {config.update_manifest}\n'
-                f'Not checking for app update.'
-            )
-            need_update, version_update = False, None
-
-        if need_update:
-            logging.info(f'Updating to version {version_update}.')
-
-            try:
-                update.perform_update()
-                logging.info(
-                    'Update was successful and the new version will be used the next time '
-                    'the app runs.'
-                )
-            except Exception as e:
-                logging.warning(f'Error updating.\n{e}')
-        else:
-            logging.info('No update required.')
-    else:
-        logging.info('Not checking for app update.')
+    app = QApplication()
+    window = ShowDialog(app, inputs, stylesheet)
+    window.show()
+    app_response = app.exec()
+    if app_response != 0:
+        logging.error(f'An error occurred. Exiting with status {app_response}.')
+        sys.exit(app_response)
 
 
 def set_config_values() -> tuple[Inputs, str | None]:
     """
     Parse CLI arguments and set ``config`` values.
     """
-    from argparse import ArgumentParser, BooleanOptionalAction, RawTextHelpFormatter
+    from argparse import ArgumentParser, RawTextHelpFormatter
 
     description = f'Show Dialog {config.version}'
 
@@ -78,28 +46,10 @@ def set_config_values() -> tuple[Inputs, str | None]:
         help='Path to JSON file that maps to the `Inputs` class.\n'
         'If both `--inputs` and `--inputs-file` are specified, `--inputs` takes precedence.',
     )
-    parser.add_argument('--stylesheet', type=str, help='Path to CSS file to apply.')
     parser.add_argument(
-        '--check-update',
-        action=BooleanOptionalAction,
-        default=True,
-        help='Check for app updates.',
-    )
-    parser.add_argument(
-        '--check-update-only',
-        action=BooleanOptionalAction,
-        default=False,
-        help='Only check for updates and do not launch the app.',
-    )
-    parser.add_argument(
-        '--update-manifest',
+        '--stylesheet',
         type=str,
-        help='App manifest file. Used to check for updates.',
-    )
-    parser.add_argument(
-        '--update-file',
-        type=str,
-        help='Path to new file to update to.',
+        help='Path to CSS file to apply.',
     )
     parser.add_argument(
         '--log-level',
@@ -122,13 +72,6 @@ def set_config_values() -> tuple[Inputs, str | None]:
         f'Show Dialog.\n  App version: {config.version}\n  Log level: {args.log_level}\n  '
         f'File: {sys.executable}'
     )
-
-    config.check_update = args.check_update
-    config.check_update_only = args.check_update_only
-    if args.update_manifest:
-        config.update_manifest = Path(args.update_manifest).resolve(strict=False)  # type: ignore
-    if args.update_file:
-        config.update_file = Path(args.update_file).resolve(strict=False)  # type: ignore
 
     # Config contents
     config_dict = {
