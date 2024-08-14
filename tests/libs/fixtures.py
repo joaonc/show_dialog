9c1a2b49a91e281d26d1f3a618731f5bbffddc4b
Joao Coelho <joaonc@users.noreply.github.com>
2024-08-13 22:03:51 -0700

    Test fixes (#31)

--- tests/libs/fixtures.py
+++ tests/libs/fixtures.py
@@ -1,37 +1,8 @@
 import pytest
-from PySide6.QtCore import QResource
 
 from src.show_dialog.inputs import Inputs
-from tests.libs import resources_rc  # noqa: F401  # Needed to initialize resources
-from tests.libs.config import TESTS_ROOT, PROJECT_ROOT
 
 
 @pytest.fixture
 def inputs_instance():
     return Inputs(title='Foo', description='Bar')
-
-
-@pytest.fixture
-def testing_resources():
-    """
-    Register the test resources file at ``tests/libs/resources_rc.py``.
-
-    Normal code uses the resources file ``src/show_dialog/ui/forms/resources_rc.py``.
-    """
-    test_resource_file = TESTS_ROOT / 'libs/resources_rc.py'
-    if not test_resource_file.is_file():
-        raise FileNotFoundError(test_resource_file)
-
-    src_resource_file = PROJECT_ROOT / 'src/show_dialog/ui/forms/resources_rc.py'
-    if not src_resource_file.is_file():
-        raise FileNotFoundError(src_resource_file)
-
-    # When registering resources, they're registered on top of (ie, in addition to) the existing
-    # ones. In order to have only the test resources, the src resources need to be unregistered
-    # first.
-
-    QResource.unregisterResource(str(src_resource_file))
-    QResource.registerResource(str(test_resource_file))
-    yield
-    QResource.unregisterResource(str(test_resource_file))
-    QResource.registerResource(str(src_resource_file))
--- tests/tests/test_utils_qt.py
+++ tests/tests/test_utils_qt.py
@@ -1,21 +1,22 @@
 import pytest
 
+from src.show_dialog.config import ASSETS_DIR
+from src.show_dialog.ui.forms import resources_rc  # noqa: F401  # Needed to initialize resources
 from src.show_dialog.utils_qt import list_resources, read_file
 from tests.libs.config import TEST_ASSETS_DIR
-from tests.libs.fixtures import testing_resources  # noqa: F401
 
 
 class TestListResources:
-    def test_resource_path(self, testing_resources):
+    def test_resource_path(self):
         files = list_resources(':/stylesheets')
-        assert files == [':/stylesheets/style_01.css']
+        assert files == [':/stylesheets/style_01.css', ':/stylesheets/style_02.css']
 
     def test_absolute_path(self):
         base_path = TEST_ASSETS_DIR / 'stylesheets'
         files = list_resources(base_path)
         assert files == [f'{base_path}/style_01.css']
 
-    def test_dir_does_not_exist_resource(self, testing_resources):
+    def test_dir_does_not_exist_resource(self):
         with pytest.raises(NotADirectoryError):
             list_resources('foo')
 
@@ -25,13 +26,13 @@ class TestListResources:
 
 
 class TestReadFile:
-    def test_read_file(self, testing_resources):
+    def test_read_file(self):
         file_content = read_file(':/stylesheets/style_01.css')
-        assert 'Test stylesheet' in file_content
+        assert 'No style applied' in file_content
 
     def test_absolute_path(self):
-        file_content = read_file(TEST_ASSETS_DIR / 'stylesheets/style_01.css')
-        assert 'Test stylesheet' in file_content
+        file_content = read_file(ASSETS_DIR / 'stylesheets/style_01.css')
+        assert 'No style applied' in file_content
 
     def test_file_does_not_exist(self):
         with pytest.raises(FileNotFoundError):
