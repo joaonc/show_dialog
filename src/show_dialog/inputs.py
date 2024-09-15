4a97c4dba12f4c09daaf70b3c7c53c44911b7804
Joao Coelho <joaonc@hotmail.com>
2024-09-15 10:59:46 -0500

    `start_server` in inputs

--- src/show_dialog/inputs.py
+++ src/show_dialog/inputs.py
@@ -51,29 +51,35 @@ class Inputs(JSONFileMixin, DefaultsMixin):
 
     dialog_title: str = ''
     """Title of the window."""
+
     title: str = ''
+
     description: str = ''
     """
     Text to display in the description section.
 
     Can be plain text or HTML. For markdown, set ``description_md`` to ``True``.
     """
+
     description_md: bool = False
     """
     Whether the text in the ``description`` field is markdown.
 
     Text in ``description`` can be plain text or HTML.
     """
+
     timeout: int = 0
     """
     Timeout in seconds for the dialog to be automatically dismissed.
 
     ``0`` for no timeout.
     """
+
     timeout_pass: bool = False
     """
     Result at the end of timeout: Pass (``True``) or fail (``False``).
     """
+
     timeout_text: str = '%v'
     """
     Text format for timeout.
@@ -89,26 +95,32 @@ class Inputs(JSONFileMixin, DefaultsMixin):
     * ``'%p%'`` to show percentage completed, ex '15%'
     * ``'%vs'`` to show the number of seconds elapsed, ex '15s'
     """
+
     buttons: Buttons = Buttons.PASS_FAIL
     """
     Set the buttons displayed.
 
     See the ``Buttons`` class for available options.
     """
+
     pass_button_text: str = ''
+
     pass_button_icon: str = ''
     """
     Set the icon for the _Pass_ button.
 
     Can be a resource path, relative path or absolute path. See examples under ``assets/inputs``.
     """
+
     fail_button_text: str = ''
+
     fail_button_icon: str = ''
     """
     Set the icon for the _Fail_ button.
 
     Can be a resource path, relative path or absolute path. See examples under ``assets/inputs``.
     """
+
     theme: Theme = Theme.Light
     """
     Theme to be applied.
@@ -116,4 +128,12 @@ class Inputs(JSONFileMixin, DefaultsMixin):
     This theme is a style and can be overridden with styling options.
 
     See the ``Theme`` class for available options.
+    """
+
+    start_server: bool = False
+    """
+    Whether to start the server for Inter-Process Communication.
+
+    This will allow the dialog to receive commands from the same or different processes.
+    Uses socket primitives.
     """
